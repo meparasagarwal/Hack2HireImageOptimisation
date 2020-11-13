ca5c4238eeae01a3c0726e93646fd586c1955f3a
Ritvik <Ritvik@Ritviks-MacBook-Air.local>
2020-11-13 17:52:28 +0530

    frontend-register button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {
+    Link
+} from "react-router-dom"
 
-function Header(){
-    return(
-        <nav class="navbar bg-dark">
-        <h1>
-          <a href="index.html"><center>Image Compressor</center></a>
-        </h1>
-        <ul>
-          <li>
-          <Link to="/register" >Register</Link></li>
-          <li>
-          <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
+function Header() {
+    return ( <
+        nav class = "navbar bg-dark" >
+        <
+        h1 >
+        <
+        a href = "index.html" > < center > Image Compressor < /center></a >
+        <
+        /h1> <
+        ul >
+        <
+        li >
+        <
+        Link to = "/login" > Login < /Link> < /
+        li > <
+        /ul> < /
+        nav >
     );
 }
 

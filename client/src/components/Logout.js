3c0c1c4f7af9c4b601fc793bdbac80383965de8c
Ritvik <Ritvik@Ritviks-MacBook-Air.local>
2020-11-15 18:11:31 +0530

    logout1

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -2,18 +2,18 @@ import React from "react";
 import {useHistory} from "react-router-dom";
 import {useEffect} from "react";
 
-function Logout(){
-    let history = useHistory();
-	useEffect(()=>{
-		fetch("/home")
-		.then((response)=>{
-			if (response.status === 201){
-				history.push("/login")
-			}else{
-                console.log(respose);
-            }
-		})
-    });
+function Logout() {
+	let history = useHistory();
+	useEffect(() => {
+		fetch("/logout")
+			.then((response) => {
+				if (response.status === 201) {
+					history.push("/login")
+				} else {
+					console.log(response);
+				}
+			})
+	});
 }
 
 export default Logout;

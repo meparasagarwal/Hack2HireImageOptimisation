d45ee15bcf8491647b6117cdee4a3ba02181832b
preethichandra20 <preethichandra30@gmail.com>
2020-11-15 23:36:55 +0530

    logout

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,17 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import {Link} from "react-router-dom";
 import {useHistory} from "react-router-dom";
 import Header from "./Header";
 import axios from "axios";
 
 function Login(){
-  //let history2 = useHistory();
-  
+          useEffect(()=>axios.get("/login")
+          .then((response)=>{})
+          .catch(err=>{
+            if(err){
+              console.log(err);
+            }
+          }))
           const [formData,setFormData]=useState({
             email:"",
             password:"",
